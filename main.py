864788bac058a83cc16e88ad6fb90ba2ad6ef77c
Tim <timailiushin@yandex.ru>
2024-01-22 19:55:49 +0300

    tubes_commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 import random
+from Tubes import Tube
 
 
 def load_image(name, colorkey=None):
@@ -14,42 +15,15 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Tube(pygame.sprite.Sprite):
-    image = load_image("tube3.png")
-    image = pygame.transform.scale(image, (100, 500))
-    image2 = pygame.transform.flip(image, 0, 1)
-
-    def __init__(self, *group, up, coordx, coordy):
-        super().__init__(*group)
-        self.image = Tube.image
-        self.rect = self.image.get_rect()
-        self.rect.x = coordx
-        self.rect.y = coordy
-        self.up = up
-
-    def update(self):
-        if self.up:
-            self.image = Tube.image2
-        else:
-            self.image = Tube.image
-
-        if self.rect.x <= -100:
-            self.rect.x = 1100
-        else:
-            self.rect.x -= v / fps
-
-
 if __name__ == '__main__':
     tubes_sprites = pygame.sprite.Group()
     x_1 = 600
     for i in range(4):
         x_1 += 300
         y_1 = random.randrange(300, 500)
-        Tube(tubes_sprites, up=False, coordx=x_1, coordy=y_1)
-        Tube(tubes_sprites, up=True, coordx=x_1, coordy=y_1 - 700)
+        Tube(tubes_sprites, up=False, coordx=x_1, coordy=y_1, v=400, fps=60)
+        Tube(tubes_sprites, up=True, coordx=x_1, coordy=y_1 - 700, v=400, fps=60)
 
-    v = 400
-    fps = 60
     pygame.init()
     size = width, height = 1100, 600
     gol_color = [150, 200, 250]
@@ -65,5 +39,5 @@ if __name__ == '__main__':
         tubes_sprites.update()
         tubes_sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(fps)
+        clock.tick(60)
     pygame.quit()
